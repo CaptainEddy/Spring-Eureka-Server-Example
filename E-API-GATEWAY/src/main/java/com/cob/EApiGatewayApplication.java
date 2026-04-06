3bb81df19e6b9d857cbef5020913ea43fa1a6477
Aishwary Dangre <aishwarydangre@gmail.com>
2026-04-06 18:16:43 +0530

    Initial commit

---
+++ E-API-GATEWAY/src/main/java/com/cob/EApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.cob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EApiGatewayApplication.class, args);
+	}
+
+}
